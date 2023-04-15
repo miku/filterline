03e38d53db557871ba6d0fb64b7098a64703f179
Martin Czygan <martin.czygan@gmail.com>
2023-04-15 12:15:53 +0200

    v0.1.5

--- filterline.c
+++ filterline.c
@@ -25,7 +25,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static const char* PROGRAM_VERSION = "0.1.4";
+static const char* PROGRAM_VERSION = "0.1.5";
 
 int main(int argc, const char* argv[])
 {
--- packaging/rpm/filterline.spec
+++ packaging/rpm/filterline.spec
@@ -1,6 +1,6 @@
 Summary:    Filter file by line number.
 Name:       filterline
-Version:    0.1.4
+Version:    0.1.5
 Release:    0
 License:    MIT
 BuildArch:  x86_64
